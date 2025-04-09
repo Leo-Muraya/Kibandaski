2f11bfd6400170e3a0833fa8f8655dd1653108e3
Elvis Kuria <levonjoroge>
2025-04-09 21:32:19 +0300

    changed footer

--- client/app/src/compenents/Footer.jsx
+++ client/app/src/compenents/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-light text-center text-lg-start">
       <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; 2024 CarWash
+        &copy; 2024 restaurant policy
       </div>
     </footer>
   );
