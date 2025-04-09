f6f6503b357a7c9d67c8fb00024bc7199866d16b
Leguminous <murayaleo911@gmail.com>
2025-04-10 00:55:03 +0300

    added a temp app.jsx and index.jsx

---
+++ client/src/app.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { AuthProvider, store } from './logic';
+import { Home, RestaurantDetail, Checkout } from './pages';
+import { Navbar } from './components';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <main className="container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/restaurants/:id" element={<RestaurantDetail />} />
+              <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </AuthProvider>
+    </Provider>
+  );
+}
